914b4c88e9c92ac15acd0aa5be942ed5a2a26803
Rohit Singhal <rohitsinghal0904@gmail.com>
2021-07-08 11:31:14 +0530

    This is first commit

---
+++ app/src/main/java/com/example/edittextapp/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.edittextapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    EditText e1;
+    Button b1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        e1=(EditText)findViewById(R.id.editText);
+        b1=(Button)findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s1=e1.getText().toString();
+                Toast.makeText(MainActivity.this, s1, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
